e52b675aba827bfeb69c3ebf51c2e25c7a9670f6
CAT-THINK\CAT <mailtopiyapong@gmail.com>
2016-11-11 10:36:43 +0700

    Modify test.php

--- production/test.php
+++ production/test.php
@@ -1,36 +1,36 @@
 <html ng-app="countryApp">
-  <head>
-    <meta charset="utf-8">
-    <title>Angular.js JSON Fetching Example</title>
-<link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- Angular -->
-<script type="text/javascript" src="../vendors/angular/angular.min.js"></script>
-    <script>
-      var countryApp = angular.module('countryApp', []);
-      countryApp.controller('CountryCtrl', function ($scope, $http){
-        $http.get('./modules/Forms/fx-data.php?limit=<?=$_GET['limit']?>').success(function(data) {
-          $scope.countries = data;
-        });
-      });
-    </script>
-  </head>
-  <body>
-<div class="container"  ng-controller="CountryCtrl">
-	<h2>Angular.js JSON Fetching Example</h2>
-        <input type="text" class="form-control input-sm" ng-model="queryString">
-        Filter by {{queryString}}
-        <table class="table table-striped table-hover">
-          <tr class="info">
-            <th>FXCode</th>
-            <th>FxName</th>
-            <th>RateToBase</th>
-          </tr>
-          <tr ng-repeat="item in countries | orderBy: 'FXCode' | filter: queryString  ">
-            <td>{{item.FXCode}}</td>
-            <td>{{item.FxName}}</td>
-            <td>{{item.RateToBase | currency: 'THB '}}</td>
-          </tr>
-        </table>
-    </div>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Angular.js JSON Fetching Example</title>
+        <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Angular -->
+        <script type="text/javascript" src="../vendors/angular/angular.min.js"></script>
+        <script>
+            var countryApp = angular.module('countryApp', []);
+            countryApp.controller('CountryCtrl', function ($scope, $http) {
+                $http.get('./modules/Forms/fx-data.php?limit=<?= $_GET['limit'] ?>').success(function (data) {
+                    $scope.countries = data;
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <div class="container"  ng-controller="CountryCtrl">
+            <h2>Angular.js JSON Fetching Example</h2>
+            <input type="text" class="form-control input-sm" ng-model="queryString">
+            Filter by {{queryString}}
+            <table class="table table-striped table-hover">
+                <tr class="info">
+                    <th>FXCode</th>
+                    <th>FxName</th>
+                    <th>RateToBase</th>
+                </tr>
+                <tr ng-repeat="item in countries| orderBy: 'FXCode' | filter: queryString  ">
+                    <td>{{item.FXCode}}</td>
+                    <td>{{item.FxName}}</td>
+                    <td>{{item.RateToBase| currency: 'THB '}}</td>
+                </tr>
+            </table>
+        </div>
+    </body>
 </html>
